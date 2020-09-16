b3fd54ee267a9f03b38b19bb6f4fbf3036f48a98
Igor Lukanin <me@igorlukanin.io>
2020-09-16 18:05:46 +0500

    Build frontend for Heroku

---
+++ frontend/build/precache-manifest.fc3b4f3f99c52dddbcd9642ff9fea77e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5351059f4865debf5b6a5e1d43c19ced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c316dc464c664b3eca3a",
+    "url": "/static/css/main.e0212933.chunk.css"
+  },
+  {
+    "revision": "b68dfc1042da9ed2cbab",
+    "url": "/static/js/3.99672644.chunk.js"
+  },
+  {
+    "revision": "4790c239734cd6787e2f12a61d4b6148",
+    "url": "/static/js/3.99672644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0efdd6ce3222931459af",
+    "url": "/static/js/4.5b6d4bd5.chunk.js"
+  },
+  {
+    "revision": "f180158677edc8a8ad86",
+    "url": "/static/js/5.59401aa8.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/5.59401aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9e43b5c390429b8ea6",
+    "url": "/static/js/6.b14850b8.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/static/js/6.b14850b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c316dc464c664b3eca3a",
+    "url": "/static/js/main.aff9b302.chunk.js"
+  },
+  {
+    "revision": "2b713506ae0b3e8c3123",
+    "url": "/static/js/runtime-main.d19da976.js"
+  },
+  {
+    "revision": "9d8cf9f7963c4594f243",
+    "url": "/static/js/xlsx.55644734.chunk.js"
+  },
+  {
+    "revision": "1bd4d740d3454c18a38944edc75a90a5",
+    "url": "/static/media/logo.1bd4d740.svg"
+  }
+]);
---
+++ frontend/build/static/js/xlsx.55644734.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpslack-vibe-frontend"]=this["webpackJsonpslack-vibe-frontend"]||[]).push([[2],{359:function(n,c){},391:function(n,c){},392:function(n,c){}}]);
+//# sourceMappingURL=xlsx.55644734.chunk.js.map
